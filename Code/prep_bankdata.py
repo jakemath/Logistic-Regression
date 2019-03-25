eb0b59c168991f83e15002fb5aa9fa35dd80ecb9
Jake Mathai <jakemathai5@gmail.com>
2019-03-25 12:22:43 -0700

    Add files via upload

--- Code/prep_bankdata.py
+++ Code/prep_bankdata.py
@@ -28,7 +28,12 @@ bank.loc[bank['job'] == 'services', 'job'] = 'blue-collar'
 transformed = bank.select_dtypes(exclude = ['number']) # Select all non-numeric data columns
 transformed = transformed.apply(LabelEncoder().fit_transform) # Transform string values into numeric values
 transformed = transformed.join(bank.select_dtypes(include = ['number'])) # Join the newly encoded columns to the rest of the frame
+for i in transformed.columns:
+    if i != 'y':
+        if ((transformed[i].dtype == 'int64' or transformed[i].dtype == 'float64') and transformed[i].mean() > 10) or i == 'cons_conf_idx':
+            transformed[i] = (transformed[i] - transformed[i].mean())/(transformed[i].std())
+
 train, test = train_test_split(transformed, test_size = 0.3) # Split data, 30% for testing
 
-print(train.to_csv(), file = open("train_bank.txt", "a"))
-print(test.to_csv(), file = open("test_bank.txt", "a"))
+print(train.to_csv(header = None, index = None), file = open("train_bank.txt", "a"))
+print(test.to_csv(header = None, index = None), file = open("test_bank.txt", "a"))
