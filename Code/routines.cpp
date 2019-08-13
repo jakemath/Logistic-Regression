c492a815d71a236cbcb7844b0955cf4e96d0baf6
Jake Mathai <jakemathai5@gmail.com>
2019-08-12 23:30:23 -0700

    Update routines.cpp

--- Code/routines.cpp
+++ Code/routines.cpp
@@ -8,96 +8,99 @@
 
 #include "routines.h"
 
-double sigmoid (double z) { return (1.0 / (1.0 + exp(-1.0 * z))); }
+// Sigmoid function
+double sigmoid(double z)
+{
+    return (1.0 / (1.0 + exp(-1.0 * z)));
+}
 
-double hypothesis (double z) { return sigmoid(z); }
-
-double dot_product (vector<double>& model, vector<double>& point)
+// Compute vector dot product
+double dot_product (vector<double>& v1, vector<double>& v2)
 {
     double total = 0.0;
-    vector<double>::iterator i = model.begin(), j = point.begin();
-    for (; i != model.end() && j != point.end(); ++i, ++j)
+    vector<double>::iterator i = v1.begin(), j = v2.begin();
+    for (; i != v1.end() && j != v2.end(); ++i, ++j)
         total += (*i) * (*j);
     return total;
 }
 
-double cost (list<point>& train_set, vector<double>& model)
+// Compute cost of model against train set
+double cost(list<vector<double>>& dataset, vector<double>& model)
 {
-    list<point>::iterator i = train_set.begin();    // Iterate through training set
-    double cost_total = 0.0, actual, predicted;   // Sum model cost for each data point
-    for (; i != train_set.end(); ++i)
+    double cost_total = 0.0;
+    double predicted;   // Sum model cost for each data point
+    list<vector<double>>::iterator i = dataset.begin(); // Iterate through training set
+    for (; i != dataset.end(); ++i)
     {
-        actual = i -> attributes.back();
-        predicted = hypothesis(dot_product(model, i -> attributes));
-        cost_total += ((-1.0 * actual * log(predicted)) - ((1.0 - actual) * log(1.0 - predicted)));
+        predicted = sigmoid(dot_product(model, *i)); // Predicted classification
+        cost_total += (-1.0 * i -> back() * log(predicted)) + (1 - i -> back()) * log(1.0 - predicted);
     }
-    return (1.0 * cost_total / train_set.size());   // Return average cost
+    return -1.0 * cost_total / dataset.size(); // Return cost per point
 }
 
-vector<double> train (list<point>& dataset, double lr)
+// Train model against train set using gradient descent,
+// with given learning rate lr
+vector<double> train(list<vector<double>>& dataset, double lr)
 {
-    vector<double> model(dataset.front().attributes.size(), 0.0);    // Create 0-filled initial model
-    vector<double>::iterator i = model.begin();
-    list<point>::iterator j;  // Iterate through dataset for each weight;
-    double train_cost = cost(dataset, model);
-    int component;
-    double gradient, hypothesized, actual;
-    while (train_cost > .0000001)    // Iterate until cost is minimized
+    vector<double> model(dataset.front().size(), 0.0); // Create 0-filled initial model
+    vector<double>::iterator i;
+    list<vector<double>>::iterator j;  // Iterate through dataset for each weight
+    double gradient;
+    double train_cost = cost(dataset, model); // Compute initial cost
+    cout << "COST: " << train_cost << endl;
+    int component; // Track component
+    for (; abs(train_cost) > .001; )    // Iterate to minimal cost
     {
-        component = 0;
+        component = 0; // Compute gradient for each component of model
+        i = model.begin();
         for (; i != model.end(); ++i)   // Update model using gradient descent
         {
             gradient = 0.0;
             j = dataset.begin();  // Iterate through dataset for each weight
             for (; j != dataset.end(); ++j)
-            {
-                hypothesized = hypothesis(dot_product(model, j -> attributes));
-                actual = j -> attributes.back();
-                gradient += (hypothesized - actual) * j -> attributes[component];
-            }
-            *i -= lr * gradient;    // Update weight
-            ++component;
+                gradient += (sigmoid(dot_product(model, *j)) - j -> back()) * (*j)[component];
+            *i -= lr * (gradient / dataset.size()); /// dataset.size());    // Update weight
+            ++component; // Next component gradient
         }
         train_cost = cost(dataset, model);  // Compute cost for updated model
         cout << "COST: " << train_cost << endl;
-        i = model.begin();  // Re-iterate
     }
     return model;   // Return trained model
 }
 
-int classify (double hypothesis, double thresh)
+int classify(double hypothesis, double thresh)
 {
     if (hypothesis >= thresh)
         return 1;
     return 0;
 }
 
-int predict (vector<double>& model, point p) { return classify(hypothesis(dot_product(model, p.attributes)), 0.996); }
-
-void split (const string& s, char c, point& p)  // Function for parsing comma separated values in a line of the txt file into a point instance
+// Function for parsing comma-separated values in a single line
+void split(const string& s, char c, vector<double>& p) 
 {
     string str;
     std::istringstream tokenStream (s);
     while (getline(tokenStream, str, ','))
-        p.attributes.push_back(stod(str));
+        p.push_back(stod(str));
 }
 
-void load_csv (std::istream& in, list<point>& frame)  // Function for loading comma separated values in txt file into a vector of points
+// Function for loading csv file into a list of vectors
+void load_csv(std::istream& in, list<vector<double>>& frame)
 {
     string str;
     while (!in.eof())
     {
-        point p;
+        vector<double> p;
         getline(in, str, '\n');
-        split(str,',',p);
+        split(str, ',', p);
         frame.push_back(p);
     }
 }
 
-std::ostream& operator << (std::ostream& out, const point& p)
+std::ostream& operator <<(std::ostream& out, const vector<double>& p)
 {
-    vector<double>::const_iterator i = p.attributes.begin();
-    for (; i != p.attributes.end(); ++i)
+    vector<double>::const_iterator i = p.begin();
+    for (; i != p.end(); ++i)
         out << *i << " ";
     return out;
 }
