d00e9e370e0a720d478b830107bb75f3f8377796
Jake Mathai <jakemathai5@gmail.com>
2021-12-12 14:18:44 -0800

    Update routines.cpp

--- source/routines.cpp
+++ source/routines.cpp
@@ -12,11 +12,9 @@
 double sigmoid(double z) { return (1.0/(1.0 + exp(-1.0*z))); }
 
 // Compute cost of model against train set
-double cost(list<vector<double>>& dataset, vector<double>& model)
-{
+double cost(list<vector<double>>& dataset, vector<double>& model) {
     double predicted, cost_total = 0.0;
-    for (list<vector<double>>::iterator datapoint = dataset.begin(); i != dataset.end(); ++i)
-    {
+    for (list<vector<double>>::iterator datapoint = dataset.begin(); i != dataset.end(); ++i) {
         predicted = sigmoid(std::inner_product(model.begin(), i->begin(), model.end(), 0.0));
         cost_total += (-1.0*i->back()*log(predicted)) + (1 - i->back())*log(1.0 - predicted);
     }
@@ -24,22 +22,18 @@ double cost(list<vector<double>>& dataset, vector<double>& model)
 }
 
 // Train model against train set using gradient descent, with given learning rate lr
-vector<double> train(list<vector<double>>& dataset, double lr)
-{
+vector<double> train(list<vector<double>>& dataset, double lr) {
     vector<double> model(dataset.front().size(), 0.0);
     vector<double>::iterator param;
     list<vector<double>>::iterator datapoint;
     int component;
     double gradient, dot_product, train_cost = cost(dataset, model);
     cout << "COST: " << train_cost << endl;
-    while (abs(train_cost) > .001)
-    {
+    while (abs(train_cost) > .001) {
         component = 0;
-        for (param = model.begin(); param != model.end(); ++param)
-        {
+        for (param = model.begin(); param != model.end(); ++param) {
             gradient = 0.0;
-            for (datapoint = dataset.begin(); datapoint != dataset.end(); ++datapoint)
-            {
+            for (datapoint = dataset.begin(); datapoint != dataset.end(); ++datapoint) {
                 dot_product = std::inner_product(model.begin(), datapoint->begin(), model.end(), 0.0)
                 gradient += (sigmoid(dot_product) - datapoint->back())*(*datapoint)[component];
             }
@@ -55,8 +49,7 @@ vector<double> train(list<vector<double>>& dataset, double lr)
 int classify(double hypothesis, double thresh) { return hypothesis >= thresh; }
 
 // Function for parsing comma-separated values in a single line
-void split(const string& s, char c, vector<double>& p) 
-{
+void split(const string& s, char c, vector<double>& p) {
     string str;
     std::istringstream tokenStream (s);
     while (getline(tokenStream, str, ','))
@@ -64,11 +57,9 @@ void split(const string& s, char c, vector<double>& p)
 }
 
 // Function for loading csv file into a list of vectors
-void load_csv(std::istream& in, list<vector<double>>& frame)
-{
+void load_csv(std::istream& in, list<vector<double>>& frame) {
     string str;
-    while (!in.eof())
-    {
+    while (!in.eof()) {
         vector<double> p;
         getline(in, str, '\n');
         split(str, ',', p);
@@ -76,8 +67,7 @@ void load_csv(std::istream& in, list<vector<double>>& frame)
     }
 }
 
-std::ostream& operator <<(std::ostream& out, const vector<double>& p)
-{
+std::ostream& operator <<(std::ostream& out, const vector<double>& p) {
     for (vector<double>::const_iterator i = p.begin(); i != p.end(); ++i)
         out << *i << " ";
     return out;
